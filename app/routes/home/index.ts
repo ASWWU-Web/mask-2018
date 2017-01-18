00af0495e9af21cc256b92613b79302837818cd3
Nolan Chinn <nolan694@gmail.com>
2017-01-17 16:05:36 -0800

    add post request functionality

--- app/routes/home/index.ts
+++ app/routes/home/index.ts
@@ -18,7 +18,8 @@ export class HomeComponent {
   test: any;
   constructor(requestService: RequestService) {
     requestService.test().then((data: any) => {this.test = data});
-    requestService.get('/profile/1617/alexander.bauer', (data) => {console.log("data",data);},null)
+    requestService.get('/profile/1617/alexander.bauer', (data) => {console.log("data",data);},null);
+    requestService.post('/login', 'hello', (data) => {console.log("postData",data);},null);
     console.log(this.test);
   }
 }
