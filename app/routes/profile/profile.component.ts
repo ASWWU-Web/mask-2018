0d360c76542b72ea467738922d11454a9d533bbc
Nolan Chinn <nolan694@gmail.com>
2017-02-07 17:52:57 -0800

    add profile-full structure to app

--- app/routes/profile/profile.component.ts
+++ app/routes/profile/profile.component.ts
@@ -4,13 +4,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ActivatedRoute, Params } from '@angular/router';
-import { RequestService } from '../../RequestService/requests'
+import { RequestService } from '../../RequestService/requests';
+
+import { ProfileFullComponent } from '../../shared/profile-full/index';
 
 @Component({
     selector: 'test-profile',
     template: `
         <h2> Name: {{profile?.full_name}}</h2>
         <p> Majors: {{profile?.majors}}</p>
+        <profile-full></profile-full>
     `,
     providers: [
         RequestService
