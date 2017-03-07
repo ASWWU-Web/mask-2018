958f3fc84dbe327064e60cf1955042b839afa703
Nolan Chinn <nolan694@gmail.com>
2017-03-06 19:00:12 -0800

    add navbar/styling WIP

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,7 @@ import {Component, NgModule} from '@angular/core';
 @Component({
   selector: 'my-app',
   template:  `
+  	<nav-bar></nav-bar>
     <h1>ASWWU Mask</h1>
     <router-outlet></router-outlet>
   `
